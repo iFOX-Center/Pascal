89eb21e9d2001289f318d74ae8918dbfbc8928ae
iFOX-Center <192953524@qq.com>
2021-03-31 13:28:23 +0800

    sss

--- new/Js/index.js
+++ new/Js/index.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+$(document).ready(function(){
+    $("#qq").click(function(){
+        window.open("https://qm.qq.com/cgi-bin/qm/qr?k=lzSO2_SviWvOZknJTBZ4vUrGKCAhtCf7&jump_from=webapi");
+    })
+    $("#email").click(function(){
+        window.open("https://qm.qq.com/cgi-bin/qm/qr?k=lzSO2_SviWvOZknJTBZ4vUrGKCAhtCf7&jump_from=webapi");
+    })
+    $("#phone").click(function(){
+        alert("暂不支持！");
+    })
+})
