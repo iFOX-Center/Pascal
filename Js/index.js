1cd999b0039a7efb2a532c4b29f5032a397f5031
iFOX-Center <192953524@qq.com>
2021-03-31 14:47:17 +0800

    ssss

--- Js/index.js
+++ Js/index.js
@@ -4,9 +4,12 @@ $(document).ready(function(){
         window.open("https://qm.qq.com/cgi-bin/qm/qr?k=lzSO2_SviWvOZknJTBZ4vUrGKCAhtCf7&jump_from=webapi");
     })
     $("#email").click(function(){
-        window.open("https://qm.qq.com/cgi-bin/qm/qr?k=lzSO2_SviWvOZknJTBZ4vUrGKCAhtCf7&jump_from=webapi");
+        window.open("http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=w7e5s7esrbqDpay7rqKqr_2grK4"");
     })
     $("#phone").click(function(){
         alert("暂不支持！");
+    })
+    $("#xyhtml").click(function(){
+        window.open("/project/XYhtml/index.html");
     })
 })
